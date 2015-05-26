ca46ea05911663bd475f418a5a36bcea2842fbcf
Gaetitan <gaetitan@gmail.com>
2015-05-26 09:51:56 +0200

    mypdo on y croit

--- model/Mypdo.class.php
+++ model/Mypdo.class.php
@@ -7,7 +7,7 @@
 			
 			
 			try{
-				$this->dbo=parent::__construct('mysql:host=127.13.150.2:3306;dbname=ruedelasoif','adminpj7zaGH','zkP4DLFTP6D2');
+				$this->dbo=parent::__construct('mysql:host=127.13.150.2:3306;dbname=ruedelasoif;','adminpj7zaGH','zkP4DLFTP6D2');
 				$req = "SET NAMES UTF8;";
 				$resu = PDO::query($req);
 			}
